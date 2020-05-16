ea0899153821809fd04ae2e2cdcce2a20869517e
Miles Brandt <mab376@mst.edu>
2020-05-16 14:07:47 -0500

    add MQTT publishing service

--- SheepDroid/app/src/main/java/com/example/sheepdroid/EditAlarmActivity.java
+++ SheepDroid/app/src/main/java/com/example/sheepdroid/EditAlarmActivity.java
@@ -75,6 +75,4 @@ public class EditAlarmActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
--- SheepDroid/app/src/main/java/com/example/sheepdroid/PrefActivity.java
+++ SheepDroid/app/src/main/java/com/example/sheepdroid/PrefActivity.java
@@ -2,6 +2,7 @@ package com.example.sheepdroid;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -9,9 +10,21 @@ import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
+import java.util.Map;
+
 public class PrefActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sh_preferences);
         SharedPreferences sheepContext = getSharedPreferences("sheepContext", MODE_PRIVATE);
@@ -45,6 +58,53 @@ public class PrefActivity extends AppCompatActivity {
                 editor.putBoolean(getString(R.string.sp_lights), v_pref_lights_b1.isChecked());
                 editor.apply();
                 finish();
+            }
+        });
+        String clientId = MqttClient.generateClientId();
+
+        final MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://192.168.0.19:1883",
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(TAG, "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d(TAG, "onFailure");
+                    Log.d(TAG, exception.toString());
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+        Button v_pref_button2 = findViewById(R.id.v_pref_button2);
+        v_pref_button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // Create new MQTT service? Maybe it will stay afloat? I'm not really sure how
+                // this works yet.
+                SharedPreferences sheepContext = getSharedPreferences("sheepContext", MODE_PRIVATE);
+                Map <String, ?> prefsMap = sheepContext.getAll();
+                // Okay, there are our variables. Now we need to publish them.
+                for(Map.Entry<String, ?> entry: prefsMap.entrySet()){
+                    String key = entry.getKey();
+                    String value = entry.getValue().toString();
+                    MqttMessage qtvalue = new MqttMessage(value.getBytes());
+                    try {
+                        client.publish(key, qtvalue);
+                        Log.d(TAG, "Message published");
+                    } catch(MqttPersistenceException e){
+                        e.printStackTrace();
+                    } catch(MqttException e){
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
